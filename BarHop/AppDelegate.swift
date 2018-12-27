7fcf8ce8e26bbe9aa09d7afe8ab4ffdda36ea1ad
Scott Macpherson <macphe22@msu.edu>
2018-12-27 14:24:46 -0500

    Embedded elements into stack view Updated pod files without stripe

--- BarHop/AppDelegate.swift
+++ BarHop/AppDelegate.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import AWSMobileClient
-import Stripe
+//import Stripe
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -23,11 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             didFinishLaunchingWithOptions: launchOptions)
     }
     
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        STPPaymentConfiguration.shared().publishableKey = "pk_test_ObmeWxmhcizqCRhpltrkOXNS"
-        // do any other necessary launch configuration
-        return true
-    }
+    //func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: //[UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //STPPaymentConfiguration.shared().publishableKey = "pk_test_ObmeWxmhcizqCRhpltrkOXNS"
+        //do any other necessary launch configuration
+        //return true
+    //}
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.

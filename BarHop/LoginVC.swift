c877f38c4b5d8d010642376c1e0aba5403d682bd
Scott Macpherson <macphe22@msu.edu>
2019-01-07 12:57:14 -0500

    Added the bars dynamoDB table into and also changed from custom AWS sign in UI to original AWS sign in UI

--- BarHop/LoginVC.swift
+++ BarHop/LoginVC.swift
@@ -11,64 +11,65 @@ import AWSAuthCore
 import AWSAuthUI
 import AWSDynamoDB
 
- class mainScreenVC: UIViewController {
+ class LoginVC: UIViewController {
 
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view, typically from a nib.
-//        if !AWSSignInManager.sharedInstance().isLoggedIn {
-//            AWSAuthUIViewController
-//                .presentViewController(with: self.navigationController!,
-//                                       configuration: nil,
-//                                       completionHandler: { (provider: AWSSignInProvider, error: Error?) in
-//                                        if error != nil {
-//                                            print("Error occurred: \(String(describing: error))")
-//                                        } else {
-//                                            // Sign in successful.
-//                                        }
-//                })
-//
-//        }
-//    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
         if !AWSSignInManager.sharedInstance().isLoggedIn {
-            presentAuthUIViewController()
+            AWSAuthUIViewController
+                .presentViewController(with: self.navigationController!,
+                                       configuration: nil,
+                                       completionHandler: { (provider: AWSSignInProvider, error: Error?) in
+                                        if error != nil {
+                                            print("Error occurred: \(String(describing: error))")
+                                        } else {
+//                                            // Sign in successful.
+                                        }
+                })
+
         }
     }
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        if !AWSSignInManager.sharedInstance().isLoggedIn {
+//            presentAuthUIViewController()
+//        }
+//    }
     
-    func presentAuthUIViewController() {
-        let config = AWSAuthUIConfiguration()
-        config.enableUserPoolsUI = true
-        //config.addSignInButtonView(class: AWSFacebookSignInButton.self)
-        //config.addSignInButtonView(class: AWSGoogleSignInButton.self)
-        
-        //change the logo on the UI here
-        //config.logoImage =
-        config.backgroundColor = UIColor.white
-        config.font = UIFont (name: "Helvetica Neue", size: 15)
-        config.isBackgroundColorFullScreen = true
-        config.canCancel = false
-        
-        
-        AWSAuthUIViewController.presentViewController(
-            with: self.navigationController!,
-            configuration: config, completionHandler: { (provider: AWSSignInProvider, error: Error?) in
-                if error == nil {
-                    // SignIn succeeded.
-                    self.createCustomer()
-                } else {
-                    // end user faced error while loggin in, take any required action here.
-                }
-        })
-    }
+//    func presentAuthUIViewController() {
+//        let config = AWSAuthUIConfiguration()
+//        config.enableUserPoolsUI = true
+//        //config.addSignInButtonView(class: AWSFacebookSignInButton.self)
+//        //config.addSignInButtonView(class: AWSGoogleSignInButton.self)
+//
+//        //change the logo on the UI here
+//        //config.logoImage =
+//        config.backgroundColor = UIColor.white
+//        config.font = UIFont (name: "Helvetica Neue", size: 15)
+//        config.isBackgroundColorFullScreen = true
+//        config.canCancel = true
+//
+//
+//        AWSAuthUIViewController.presentViewController(
+//            with: self.navigationController!,
+//            configuration: config, completionHandler: { (provider: AWSSignInProvider, error: Error?) in
+//                if error == nil {
+//                    // SignIn succeeded.
+//                    self.createCustomer()
+//                } else {
+//                    // end user faced error while loggin in, take any required action here.
+//                }
+//        })
+//    }
 
 
     @IBAction func signOutButtonPress(_ sender: Any) {
         print("here")
         AWSSignInManager.sharedInstance().logout(completionHandler: {(result: Any?, error: Error?) in
-            self.presentAuthUIViewController()
-            // print("Sign-out Successful: \(signInProvider.getDisplayName)");
+            self.viewDidLoad()
+            //self.presentAuthUIViewController()
+           // print("Sign-out Successful: \(signInProvider.getDisplayName)");
             
         })
     }
---
+++ BarHop/RegisterBarVC.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterBarVC.swift
+//  BarHop
+//
+//  Created by Scott Macpherson on 1/6/19.
+//  Copyright © 2019 Scott Macpherson. All rights reserved.
+//
+
+import UIKit
+
+class RegisterBarVC: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

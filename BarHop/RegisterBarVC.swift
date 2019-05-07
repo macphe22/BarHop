53234fa248674e402de5248293b4afd6c1fd79e2
jlandy <jlandy@umich.edu>
2019-05-06 20:45:50 -0400

    all other files

--- BarHop/RegisterBarVC.swift
+++ BarHop/RegisterBarVC.swift
@@ -12,32 +12,54 @@ class RegisterBarVC: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("register view loaded")
-        
         // Do any additional setup after loading the view.
     }
+    
     //Textfields below
-    
-    @IBOutlet weak var bar_name: UITextField!
-    @IBOutlet weak var ownersName: UITextField!
-
-    @IBOutlet weak var phoneNumber: UITextField!
-    
+    @IBOutlet weak var name: UITextField!
+    @IBOutlet weak var ownerName: UITextField!
     @IBOutlet weak var address: UITextField!
-    @IBOutlet weak var maxCapacity: UITextField!
-    @IBOutlet weak var accountNumber: UITextField!
+    @IBOutlet weak var city: UITextField!
+    @IBOutlet weak var state: UITextField!
+    @IBOutlet weak var zip: UITextField!
+    @IBOutlet weak var maxTicketsPerNight: UITextField!
+    @IBOutlet weak var ticketCost: UITextField!
+    @IBOutlet weak var phoneNumber: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var venmoUsername: UITextField!
+    @IBOutlet weak var errorLabel: UILabel!
     
-    @IBOutlet weak var ticketPrice: UITextField!
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Insert a row into the aws database for bars when submit is clicked
+    @IBAction func submitClicked(_ sender: Any) {
+        // First we need to verify all user info
+        // Check that the maxTickets and ticketCost are above zero
+        while true {
+            var correct = true
+            let maxTickets = Int(maxTicketsPerNight.text!)
+            let cost = Int(ticketCost.text!)
+            if maxTickets! < 0 {
+                maxTicketsPerNight.text = ""
+                correct = false
+                // Show the error message
+                errorLabel.isHidden = false
+            }
+            if cost! < 0 {
+                ticketCost.text = ""
+                correct = false
+                // Show the error message
+                errorLabel.isHidden = false
+            }
+            // If max tickets and cost check out, we can break from the loop
+            if correct == true {
+                errorLabel.isHidden = true
+                break
+            }
+        }
+        
+        // Now we know that the parameters are (likely) correct, we will instantiate a row in our table and insert it with all of the bars information
+        
+        
+        
+        
     }
-    */
-
 }

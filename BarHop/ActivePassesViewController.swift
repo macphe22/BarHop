03e64df7d6bfe026b61b2f5bf55bf33ba1147d14
Scott Macpherson <macphe22@msu.edu>
2019-08-03 14:15:21 -0400

    Implemented dispatch group to make the call to the UI more async

--- BarHop/ActivePassesViewController.swift
+++ BarHop/ActivePassesViewController.swift
@@ -7,28 +7,78 @@
 //
 
 import UIKit
+import AWSDynamoDB
+import AWSAuthCore
+
 
 class ActivePassesViewController: UIViewController {
     @IBOutlet weak var activePassesLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    
+    let dispatchGroup = DispatchGroup();
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        createDataArray()
-        // Do any additional setup after loading the view.
+        getCustomersActiveTrips()
+        dispatchGroup.notify(queue: .main)
+        {
+            self.displayPasses()
+        }
     }
     
-    //MARK: - Outlets
+    func displayPasses(){
+        self.tableView?.reloadData();
+    }
 
-
-
-    
     //Mark: - Data
     var activePasses = [String]()
-    
+    var returnedActivePasses = Set<String>();
     func createDataArray(){
         activePasses = ["Harpers", "Ricks", "Pt O'Malies"]
+    }
+    func getCustomersActiveTrips(){
+        // Create a query expression
+        self.dispatchGroup.enter();
+        var returnedPasses = [String]();
+        let dynamoDbObjectMapper = AWSDynamoDBObjectMapper.default()
+        let customerItem: Customer = Customer();
+        customerItem._userId = AWSIdentityManager.default().identityId
+        customerItem._tripsTaken = 0
+        dynamoDbObjectMapper.load(Customer.self,
+                                  hashKey: customerItem._userId,
+                                  rangeKey: customerItem._tripsTaken,
+                                  completionHandler: {
+                                    (objectModel: AWSDynamoDBObjectModel?, error: Error?) -> Void in
+                                    if let error = error {
+                                        print("Amazon DynamoDB Read Error: \(error)")
+                                        return
+                                    }
+                                    else if let loadedCustomer = objectModel as? Customer{
+                                        
+                                        self.returnedActivePasses = loadedCustomer._activeTrips ?? Set<String>()
+                                        //print(self.returnedActivePasses)
+                                        returnedPasses = self.mapReturnedActivePassesToActivePassesArray();
+                                        self.activePasses = returnedPasses
+                                    }
+                                    print("An item was read")
+                                    self.dispatchGroup.leave()
+        
+                                    
+        })
+        
+        
+        
+    }
+    
+    func mapReturnedActivePassesToActivePassesArray() -> [String]{
+        var returnArray = [String]();
+        for activePass in self.returnedActivePasses{
+            returnArray.append(activePass);
+            
+        }
+        
+        return returnArray
     }
 
 }

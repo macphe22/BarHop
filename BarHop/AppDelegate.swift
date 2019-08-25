d5c4d76f52c906c41c9aa361fde5cbd2f24fb736
John Landy <johnlandy09@gmail.com>
2019-08-24 20:40:06 -0400

    cleaning up files

--- BarHop/AppDelegate.swift
+++ BarHop/AppDelegate.swift
@@ -48,15 +48,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 annotation: annotation)
         }
     }
-    // Add a AWSMobileClient call in application:didFinishLaunching
-
     
-    //func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: //[UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //STPPaymentConfiguration.shared().publishableKey = "pk_test_ObmeWxmhcizqCRhpltrkOXNS"
-        //do any other necessary launch configuration
-        //return true
-    //}
-
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -78,7 +70,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 

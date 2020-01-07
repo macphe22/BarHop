50f5568c82395e170c99a125dd2a62b931db19df
Scott Macpherson <macphe22@msu.edu>
2020-01-06 19:11:43 -0500

    Changed UI stuff but still need to fix the database query

--- BarHop/RedeemPassViewController.swift
+++ BarHop/RedeemPassViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
-
+import AWSAuthUI
+import AWSDynamoDB
 class RedeemPassViewController: UIViewController {
     var barName: String? = ""
+    let dispatchGroup = DispatchGroup()
     @IBAction func redeemButton(_ sender: UIButton) {
+        removePassFromCustomer(bar: barName!)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,7 +48,39 @@ class RedeemPassViewController: UIViewController {
         self.barNameLabel.textColor = .white
     }
     
-
+    func removePassFromCustomer(bar: String)
+    {
+        // This retreival will decrement the number of active passes for the logged in user
+        // This first block is handling the retreival of the current customer in the database
+        print(bar)
+        self.dispatchGroup.enter()
+        let customerItem: Customer = Customer()
+        let dynamoDBObjectMapper = AWSDynamoDBObjectMapper.default()
+        customerItem._userId = AWSIdentityManager.default().identityId
+        dynamoDBObjectMapper.load(Customer.self, hashKey: customerItem._userId!,
+                                  rangeKey: nil, completionHandler: {
+                                    (objectModel: AWSDynamoDBObjectModel?, error: Error?) -> Void in
+                                    if let error = error {
+                                        print("Amazon DynamoDB Read Error: \(error)")
+                                        return
+                                    }
+                                    else if let customer = objectModel as? Customer {
+                                        // We can now remove the active pass from the customer
+                                        print(customer._activeTrips ?? Set<String>())
+                                        if (((customer._activeTrips?.contains(bar))!)) {
+                                            customer._activeTrips?.remove(bar)
+//                                            print(customer._activeTrips ?? Set<String>())
+                                        }
+                                        // Now that we have retreived the Customer, we can save it back into the database
+                                        dynamoDBObjectMapper.save(customer).continueWith(block: { (task:AWSTask<AnyObject>!) -> Void in
+                                            if let error = task.error as NSError? {
+                                                print("The request failed. Error: \(error)")
+                                            }
+                                        })
+                                        self.dispatchGroup.leave()
+                                    }
+        })
+    }
     /*
     // MARK: - Navigation
 
---
+++ BarHop/RedeemedPopUpViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RedeemedPopUpViewController.swift
+//  BarHop
+//
+//  Created by Scott Macpherson on 12/26/19.
+//  Copyright © 2019 Scott Macpherson. All rights reserved.
+//
+
+import UIKit
+
+class RedeemedPopUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        UIModalPresentationStyle.pageSheet
+        // self.popoverPresentationController?.backgroundColor = UIColor.black
+        // self.preferredContentSize = CGSize(width: 200, height: 300)
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var CloseButton: UIButton!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
